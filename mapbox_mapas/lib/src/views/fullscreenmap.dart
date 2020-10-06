923aac0b59479e057f575967b541a5214ac049ef
Marioagtzc <marioagtzc@hotmail.com>
2020-10-06 17:01:20 -0500

    End of section 18 plus

--- mapbox_mapas/lib/src/views/fullscreenmap.dart
+++ mapbox_mapas/lib/src/views/fullscreenmap.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 class FullScreenMap extends StatefulWidget {
@@ -15,6 +18,23 @@ class _FullScreenMapState extends State<FullScreenMap> {
 
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+    _onStyleLoaded();
+  }
+
+  void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/custom-icon.png");
+    addImageFromUrl("networkImage", "https://via.placeholder.com/50");
+  }
+
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController.addImage(name, list);
+  }
+
+  Future<void> addImageFromUrl(String name, String url) async {
+    var response = await http.get(url);
+    return mapController.addImage(name, response.bodyBytes);
   }
 
   @override
@@ -29,7 +49,20 @@ class _FullScreenMapState extends State<FullScreenMap> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: <Widget>[
-        // Zoomin
+        // Símbolos
+        FloatingActionButton(
+          child: Icon(Icons.sentiment_very_dissatisfied),
+          onPressed: () {
+            mapController.addSymbol(SymbolOptions(
+              geometry: center,
+              iconImage: 'networkImage',
+              textField: 'Montaña creada aquí',
+              textOffset: Offset(0, 2)
+            ));
+          }
+        ),
+        SizedBox(height: 5,),
+        // ZoomIn
         FloatingActionButton(
           child: Icon(Icons.zoom_in),
           onPressed: () {
@@ -37,7 +70,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
           }
         ),
         SizedBox(height: 5,),
-        // Zoomout
+        // ZoomOut
         FloatingActionButton(
           child: Icon(Icons.zoom_out),
           onPressed: () {
@@ -55,6 +88,7 @@ class _FullScreenMapState extends State<FullScreenMap> {
               } else {
                 selectedStyle = oscuroStyle;
               }
+              _onStyleLoaded();
             });
           }
         )
